89cf073cb54989b621612d7e60214b7b698284e6
Victor Borja <vborja@apache.org>
2018-03-28 11:39:30 -0600

    Document lib/pond

--- lib/pond.ex
+++ lib/pond.ex
@@ -1,12 +1,39 @@
 defmodule Pond do
-  @moduledoc """
+  @moduledoc ~S"""
   Pond.
 
-  `use Pond`
+  `import Pond`
 
   See the README.md file for an usage guide.
   """
 
+  @doc ~S"""
+  Create a *pond* from an initial state and a handler function.
+
+  The function returned by `pond/2` is just `func` with its
+  first two arguments already applied.
+
+  That is, `func`'s minimal arity is 2.
+
+  All remaining arguments are supplied by the user when calling
+  the result of `pond/2`.
+
+      iex> import Pond
+      ...> f = pond(:hello, fn
+      ...>   pond, state = :hello ->
+      ...>     {state, pond.(:world)}
+      ...>   pond, state ->
+      ...>     {state, pond.(state)}
+      ...> end)
+      ...>
+      ...> assert {:hello, f} = f.()
+      ...> assert {:world, f} = f.()
+      ...>
+      ...> :erlang.fun_info(f)[:arity]
+      0
+
+  """
+  @spec pond(state :: any(), func :: function()) :: function()
   def pond(state, func) when is_function(func) do
     arity = :erlang.fun_info(func)[:arity]
     pond_fix(arity, func).(state)
